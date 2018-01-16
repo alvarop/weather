584d4042d22bea2f0454a4d575e827e0594053d9
Alvaro Prieto <alvaro@alvarop.com>
2018-01-15 21:23:32 -0800

    Add initial BLE advertising option
    Only pressure and temperature are available at the moment

--- fw/controller/apps/weather/src/main.c
+++ fw/controller/apps/weather/src/main.c
@@ -11,16 +11,84 @@
 #include <sensor/temperature.h>
 #include <sensor/pressure.h>
 
+#if MYNEWT_VAL(USE_BLE)
+#include <host/ble_hs.h>
+#endif
+
 #include "sysinit/sysinit.h"
 #include "os/os.h"
 #include "bsp/bsp.h"
 #include "hal/hal_gpio.h"
 
 /* Define task stack and task object */
-#define WEATHER_TASK_PRI         (OS_TASK_PRI_HIGHEST) 
+#define WEATHER_TASK_PRI         (10) 
 #define WEATHER_STACK_SIZE       (256) 
 struct os_task weather_task; 
 os_stack_t weather_task_stack[WEATHER_STACK_SIZE]; 
+
+#define BEACON_MAGIC 0xDA7A
+
+typedef struct {
+    int16_t magic;
+    uint16_t timestamp;
+    int16_t temperature;
+    int16_t light;
+    float pressure;
+    uint16_t wind;
+    uint16_t rain;
+} __attribute__((packed)) weather_beacon_t;
+
+#if MYNEWT_VAL(USE_BLE)
+weather_beacon_t beacon_data;
+
+static void ble_app_advertise();
+
+static void ble_app_set_addr(void) {
+    ble_addr_t addr;
+    int rc;
+
+    rc = ble_hs_id_gen_rnd(1, &addr);
+    assert(rc == 0);
+
+    rc = ble_hs_id_set_rnd(addr.val);
+    assert(rc == 0);
+}
+
+static int bleprph_gap_event(struct ble_gap_event *event, void *arg) {
+    switch (event->type) {
+    case BLE_GAP_EVENT_ADV_COMPLETE:
+        ble_app_advertise();
+        return 0;
+    }
+
+    console_printf("Other event: %d\n", event->type);
+    return 0;
+}
+
+
+static void ble_app_advertise() {
+    struct ble_gap_adv_params adv_params;
+    struct ble_hs_adv_fields fields;
+    int rc;
+
+    fields = (struct ble_hs_adv_fields){ 0 };
+    rc = ble_eddystone_set_adv_data_uid(&fields, &beacon_data);
+    assert(rc == 0);
+
+    adv_params = (struct ble_gap_adv_params){ 0 };
+    rc = ble_gap_adv_start(BLE_OWN_ADDR_RANDOM, NULL, 1000,
+                           &adv_params, bleprph_gap_event, NULL);
+    assert(rc == 0);
+}
+
+static void ble_app_on_sync(void) {
+    /* Generate a non-resolvable private address. */
+    ble_app_set_addr();
+
+    /* Advertise indefinitely. */
+    ble_app_advertise();
+}
+#endif
 
 typedef struct {
     float temperature;
@@ -72,6 +140,10 @@ void print_bmp_data() {
         frac_part = (int)((data.pressure/1000.0 - int_part) * 100000);
         
         console_printf("Pressure: %d.%dkPa\n", int_part, frac_part);
+
+        beacon_data.temperature = (int16_t)(data.temperature * 100);
+        beacon_data.pressure = data.pressure;
+
     } else {
         console_printf("Error reading from BMP280\n");
     }
@@ -83,6 +155,16 @@ void weather_task_func(void *arg) {
 
     hal_gpio_init_out(LED_BLINK_PIN, 1);
 
+#if MYNEWT_VAL(USE_BLE)
+    beacon_data.magic = 0xDA7A;
+    beacon_data.timestamp = 0;
+    beacon_data.temperature = 0;
+    beacon_data.light = 0;
+    beacon_data.pressure = 0.0;
+    beacon_data.wind = 0;
+    beacon_data.rain = 0;
+#endif
+
     struct adc_dev *adc;
 
     adc = adc_init();
@@ -91,9 +173,9 @@ void weather_task_func(void *arg) {
     temt6000_init(adc);
 
     while (1) {
-        print_bmp_data();
+        os_time_delay(OS_TICKS_PER_SEC * 5);
 
-        os_time_delay(OS_TICKS_PER_SEC);
+        print_bmp_data();
 
         hal_gpio_toggle(LED_BLINK_PIN);
     }
@@ -114,6 +196,10 @@ int main(int argc, char **argv) {
         OS_WAIT_FOREVER,
         weather_task_stack,
         WEATHER_STACK_SIZE);
+
+#if MYNEWT_VAL(USE_BLE)
+     ble_hs_cfg.sync_cb = ble_app_on_sync;
+#endif
 
     while (1) {
         os_eventq_run(os_eventq_dflt_get());
